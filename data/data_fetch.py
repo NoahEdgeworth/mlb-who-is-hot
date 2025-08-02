58dd298be78e2d4b7b70dd255b129dd152a905a2
Noah Edgeworth <nhedgeworth@gmail.com>
2025-08-01 20:35:57 -0400

    Updated the Data Fetch to show previous month until a week into current month

--- data/data_fetch.py
+++ data/data_fetch.py
@@ -8,11 +8,13 @@ def get_monthly_batting_stats():
     today = date.today()
     first_of_month = today.replace(day=1)
 
-    if today == first_of_month:
-        last_month = today.replace(day=1) - timedelta(days=1)
+    if today.day <= 6:
+        # Use last full month if we are in the first week of new month
+        last_month = first_of_month - timedelta(days=1)
         start_date = last_month.replace(day=1)
         end_date = last_month
     else:
+        # Else, use current month
         start_date = first_of_month
         end_date = today
     return batting_stats_range(start_dt=str(start_date), end_dt=str(end_date))
---
+++ pages/Stats_By_Team.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from data.data_clean import batting_df
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.write('Coming Soon...')
